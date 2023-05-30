65d1ea3b2bd07a34c7b0e1839516bf58d7c80875
Tanya <Tanya2204frosya@gmail.com>
2023-05-30 17:12:06 +0300

    task

---
+++ task2.java
@@ -0,0 +1,16 @@
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1001; i++) {
+            boolean isPrime = true;  
+            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++){
+                if ((i % j) == 0) { 
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
